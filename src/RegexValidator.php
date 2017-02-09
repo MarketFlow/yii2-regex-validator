45f798e9fe26c594e0f28cbce858e3450e8d5288
Joey Claessen <joey_claessen@hotmail.com>
2017-02-09 09:30:06 +0100

    Support different wraps, fixed return of function

--- src/RegexValidator.php
+++ src/RegexValidator.php
@@ -1,23 +1,21 @@
 <?php
 
-
 namespace MarketFlow\Yii2\Validators;
-
 
 use \yii\validators\Validator;
 
 class RegexValidator extends Validator
 {
-    public $wrap = false;
+    public $wrap = '';
 
     protected function validateValue($value)
     {
         try {
-            preg_match($this->wrap ? "/$value/" : $value, '');
+            preg_match($this->wrap  . $value . $this->wrap, '');
         } catch (\Exception $e) {
-            return \Yii::t('app', 'Invalid regex for {attribute}');
+            return [\Yii::t('app', 'Invalid regex for {attribute}'),  []];
         }
+
+        return null;
     }
-
-
 }
